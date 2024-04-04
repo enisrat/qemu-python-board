aab44f040cf07f00f859ff3a01226d1e34ba2973
enisrat <84377236+enisrat@users.noreply.github.com>
2024-04-04 15:09:22 +0200

    [fix] CPUArchState regs[16] and xregs[32] explicit names for SWIG

--- target/arm/cpu.h
+++ target/arm/cpu.h
@@ -232,14 +232,70 @@ typedef struct NVICState NVICState;
 
 typedef struct CPUArchState {
     /* Regs for current mode.  */
-    uint32_t regs[16];
+    union{
+        uint32_t regs[16];
+        struct {
+            uint32_t r0;
+            uint32_t r1;
+            uint32_t r2;
+            uint32_t r3;
+            uint32_t r4;
+            uint32_t r5;
+            uint32_t r6;
+            uint32_t r7;
+            uint32_t r8;
+            uint32_t r9;
+            uint32_t r10;
+            uint32_t r11;
+            uint32_t r12;
+            uint32_t r13;
+            uint32_t r14;
+            uint32_t r15;
+        };
+    };
 
     /* 32/64 switch only happens when taking and returning from
      * exceptions so the overlap semantics are taken care of then
      * instead of having a complicated union.
      */
     /* Regs for A64 mode.  */
-    uint64_t xregs[32];
+    union{
+        uint64_t xregs[32];
+        struct {
+            uint64_t x0;
+            uint64_t x1;
+            uint64_t x2;
+            uint64_t x3;
+            uint64_t x4;
+            uint64_t x5;
+            uint64_t x6;
+            uint64_t x7;
+            uint64_t x8;
+            uint64_t x9;
+            uint64_t x10;
+            uint64_t x11;
+            uint64_t x12;
+            uint64_t x13;
+            uint64_t x14;
+            uint64_t x15;
+            uint64_t x16;
+            uint64_t x17;
+            uint64_t x18;
+            uint64_t x19;
+            uint64_t x20;
+            uint64_t x21;
+            uint64_t x22;
+            uint64_t x23;
+            uint64_t x24;
+            uint64_t x25;
+            uint64_t x26;
+            uint64_t x27;
+            uint64_t x28;
+            uint64_t x29;
+            uint64_t x30;
+            uint64_t x31;
+        };
+    };
     uint64_t pc;
     /* PSTATE isn't an architectural register for ARMv8. However, it is
      * convenient for us to assemble the underlying state into a 32 bit format
